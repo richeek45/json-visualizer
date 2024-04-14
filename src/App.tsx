b8fb7ea7747278108ddf0016d0cecd064418b163
richeek45 <richeek45@gmail.com>
2024-04-14 22:10:18 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>Hi there. Start of tree ds... json</div>
+    </>
+  )
+}
+
+export default App
